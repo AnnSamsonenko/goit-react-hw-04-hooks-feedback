f55cdf8a42b7828da7f592cd7a637f1f5b40dd82
Anna Samsonenko <foster.anna93@gmail.com>
2022-01-30 14:21:11 +0200

    replacing state for individual cases

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,25 +7,36 @@ import { Statistics } from "./Statistics/Statistics";
 import { Notification } from "./Notification/Notification";
 
 export const App = () => {
-  const [state, setState] = useState({ good: 0, neutral: 0, bad: 0 });
-  const { good, bad, neutral } = state;
-  const options = Object.keys(state);
+  const [good, setGood] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [neutral, setNeutral] = useState(0);
 
   const onLeaveFeedback = (e) => {
     const key = e.target.name;
-    setState((prevState) => {
-      return { ...prevState, [key]: prevState[key] + 1 };
-    });
+    switch (key) {
+      case "good":
+        setGood(good + 1);
+        break;
+      case "neutral":
+        setNeutral(neutral + 1);
+        break;
+      case "bad":
+        setBad(bad + 1);
+        break;
+      default:
+        return;
+    }
   };
 
   const countTotalFeedback = () => {
-    return good + bad + neutral;
+    return Number(good) + Number(bad) + Number(neutral);
   };
 
   const countFeedbackPercentage = (feedbackCount) => {
     return Math.floor((feedbackCount * 100) / countTotalFeedback());
   };
 
+  const options = Object.keys({ good, bad, neutral });
   const totalStats = countTotalFeedback();
   const positiveStats = countFeedbackPercentage(good);
   return (
